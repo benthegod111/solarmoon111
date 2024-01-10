df90b9d10b17cfaf8e85cedd79e24cd933651900
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-01-10 23:25:05 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,89 @@
+// App.jsx
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import sound from './assets/music/music.mp3';
+import hawking from './assets/btc.png';
+import aboutImg from './assets/approved.png';
+import telegram from './assets/telegram.svg';
+import twitter from './assets/twitter.svg';
+import { useTypingEffect } from './hooks/typing-effect';
+import Overlay from './components/Overlay';
+import { omit } from 'lodash';
+
+function App() {
+  const contract = 'TBA';
+
+
+  const playAudio = () => {
+    const audio = document.querySelector('.audio');
+    if (audio) {
+      audio.play();
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="insideApp">
+        <Overlay playAudio={playAudio} />
+        <audio autoPlay className="audio" loop>
+          <source src={sound} type="audio/mp3" />
+          Your browser does not support the audio element.
+        </audio>
+        <div className="mainSection">
+          <div className="mainMargin">
+            <div className="imgBlock">
+              <img className="hawkingImg" alt="btcetf" src={hawking} />
+            </div>
+            <div className="textBlock">
+              <div className="caWidget">
+                <p className="caWidgetText">CA: {contract}</p>
+              </div>
+              <div className="headingDiv">
+                <h1 className="heading">
+                  BITCOIN ETF <br />
+                  <span className="whiteText">FINALLY APPROVED</span>
+                </h1>
+                <a href="#">
+                  <button class="button mainButton">
+                    JET THE $BTCETF
+                  </button>
+                </a>
+                <div className="socialsBlock">
+                  <h3 className="socialsText whiteText">SOCIALS:</h3>
+                  <img className="socialsImg" src={telegram} alt="telegram"/>
+                  <img className="socialsImg" src={twitter} alt="twitter"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="aboutSection">
+          <div className="about-inside">
+            <div className="textBlock">
+              <h2 className="heading2">
+                BREAKING NEWS:<br/>
+                BTC ETF FINALLY <br/>
+                APPROVED BY SEC
+              </h2>
+              <p className="bodyText">
+              BTC APPORVED FINALLY BTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLYBTC APPORVED FINALLY
+              </p>
+            </div>
+            <div className="imgBlock">
+              <img className="aboutImg" src={aboutImg} alt="STEPHEN HAWKING ON THE VACATION"/>
+            </div>
+          </div>
+        </div>
+        <div className="imgSection">
+          <div className="runningTextBlock">
+            <h1 className="heading runningText">
+              $HAWSEX<span className="whiteText">$BTCETF</span>$BTCETF<span className="whiteText">$BTCETF</span>$BTCETF<span className="whiteText">$BTCETF</span>$BTCETF<span className="whiteText">$BTCETF</span>$BTCETF<span className="whiteText">$BTCETF</span>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Overlay.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import dogNoBg from '../assets/btc.png';
+
+export default function Overlay({ playAudio }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const [animateImages, setAnimateImages] = useState(false);
+
+  useEffect(() => {
+    if (animateImages) {
+      // Trigger animation by setting translateY and flip vertically with ease transition
+      document.querySelectorAll('.rocket').forEach((rocket) => {
+        if (rocket.classList.contains('rocketFlip')) {
+          // If the rocket has the 'rocketFlip' class, maintain the flipped state
+          rocket.style.transition = 'transform 1s ease'; // Adjust the duration and easing as needed
+          rocket.style.transform = 'translateY(-1000px) translateX(-1000px) scaleY(-1)';
+        } else {
+          // If the rocket doesn't have the 'rocketFlip' class, apply the default transform
+          rocket.style.transition = 'transform 1s ease'; // Adjust the duration and easing as needed
+          rocket.style.transform = 'translateY(-1000px) translateX(1000px)';
+        }
+      });
+    }
+  }, [animateImages]);
+
+  function toggle() {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+    playAudio();
+
+    // Start animation when CONTINUE is clicked
+    setAnimateImages(true);
+
+    // Reset animation after a delay (adjust as needed)
+    setTimeout(() => {
+      setAnimateImages(false);
+      document.querySelectorAll('.rocket').forEach((rocket) => {
+        rocket.style.transform = 'translateY(0)';
+      });
+    }, 1500); // Adjust the delay based on your animation duration
+  }
+
+  return (
+    <>
+      <div
+        style={{
+          opacity: isOpen ? 1 : 0,
+          maxHeight: isOpen ? '1000px' : '0',
+          overflow: 'hidden',
+          transition: 'opacity 1s ease, max-height 1s ease',
+        }}
+        className="overlay"
+      >
+        <div className="overlay-inside">
+          <h1 className="heading overlayText">
+            <span className="whiteText">Stephen Hawking</span> <br/>Caribbean Island
+          </h1>
+          <div className="imagesOverlay">
+            <div>
+              <img className="rocket rocketLeft rocketFlip" style={{
+                transition: 'transform 0.5s ease'
+              }} src="https://media.tenor.com/GocCvG7hs78AAAAi/rocket-joypixels.gif" alt="rocket" />
+            </div>
+            <div className="spinBlock">
+              <img className="dogNoBg" alt="dog" src={dogNoBg} />
+            </div>
+            <div>
+              <img className="rocket" style={{
+                transition: 'transform 0.5s ease'
+              }} src="https://media.tenor.com/GocCvG7hs78AAAAi/rocket-joypixels.gif" alt="rocket" />
+            </div>
+          </div>
+          <div>
+            <button onClick={toggle} className="button">
+              ENTER THE ISLAND
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
